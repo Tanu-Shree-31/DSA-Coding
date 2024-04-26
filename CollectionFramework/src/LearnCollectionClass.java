fab3eb37215752a2ebb93b9eae9f367bce4938fb
Tanushree Shetty <69342524+Tanu-Shree-31@users.noreply.github.com>
2024-04-26 23:13:18 +0530

    collection class

--- CollectionFramework/src/LearnCollectionClass.java
+++ CollectionFramework/src/LearnCollectionClass.java
@@ -1,9 +1,23 @@
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class LearnCollectionClass {
     public static void main(String[] args) {
         List<Integer> list = new LinkedList<>();
+        list.add(12);
+        list.add(23);
+        list.add(56);
+        list.add(89);
+        list.add(32);
+        list.add(56);
 
+        System.out.println("Min element: "+ Collections.min(list));
+        System.out.println("Max element: "+ Collections.max(list));
+        System.out.println(Collections.frequency(list, 56));
+
+        Collections.sort(list, Comparator.reverseOrder());
+        System.out.println(list);
     }
 }
